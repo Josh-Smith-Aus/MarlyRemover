a06b4d5bdaee429051759286a407cf049e70f75e
Josh-Smith-Aus <joshuasmith_28@hotmail.com>
2022-02-06 10:00:43 +1100

    Update main.py

--- main.py
+++ main.py
@@ -36,12 +36,15 @@ async def on_message(message):
     msg_content = message.content.lower()
     marlyWord = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
     Marly = 332674954958995466 #Marlys user ID = 332674954958995466
+    RoleIdGloom = 928262721550565417 #gloom role id
+    
+
 
     # check word if match with the list
-    if any(word in msg_content for word in marlyWord) and message.author.id == Marly and message.channel.id == gloomChannel:
+    if any(word in msg_content for word in marlyWord) and message.author.id not in RoleIdGloom and message.channel.id == gloomChannel:
         await message.channel.send('Bad Marly! Leave them to play GloomHaven')
         await message.delete(delay =3)
-    elif not all(word in msg_content for word in marlyWord) and message.author.id == Marly and message.channel.id == gloomChannel:
+    elif not all(word in msg_content for word in marlyWord) and message.author.id not in RoleIdGloom and message.channel.id == gloomChannel:
         #Change Marlys comments to random upper lower case
         await message.channel.send(''.join([char.lower() if random.randint(0,1) else char.upper() \
                    for char in message.content]))
