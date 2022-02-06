2f0a5a39dde0dc845bdc2f3b7b3592d28e5f145c
Josh-Smith-Aus <joshuasmith_28@hotmail.com>
2022-02-06 14:26:57 +1100

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,8 @@ async def on_message(message):
     roleIdGloom = 931329921190608927 #gloom role id 931329921190608927 
     role = message.guild.get_role(roleIdGloom)
     listForGloomVotes = []
-   
+    count = 0
+
     if message.channel.id == gloomChannel and message.author != client.user:
    
         # check word if match send('Bad Marly! Leave them to play GloomHaven')
@@ -57,12 +58,16 @@ async def on_message(message):
                         for char in message.content]))
             await message.delete(delay =3)
     
-        #Add up?
+        #Add up
         elif any(word in msg_content for word in marlyWord) and role in message.author.roles:
-            listForGloomVotes += message.content
-            sleep (5)
-            await message.channel.send('The most common answer is % s' %(multimode(listForGloomVotes)))
-            listForGloomVotes.clear()
+            while count < 3:
+                listForGloomVotes += message.content
+                count += 1
+                if count >= 4:
+                    await message.channel.send('The most common answer is ' %(multimode(listForGloomVotes)))
+                    listForGloomVotes.clear()
+                    count = 0
+                    return
     
     #stop cash using main channel to vote in gloom
     if message.channel.id == mainChannelId and message.author == cashensId and any(word in msg_content for word in marlyWord):
